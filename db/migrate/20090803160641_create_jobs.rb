5f5dc74a73b463e70c083972411b9eb7e2d6bef2
Michael Siebert <siebertm85@googlemail.com>
2009-08-04 16:15:21 +0200

    create jobber_jobs tables

--- db/migrate/20090803160641_create_jobs.rb
+++ db/migrate/20090803160641_create_jobs.rb
@@ -1,12 +1,20 @@
 class CreateJobs < ActiveRecord::Migration
   def self.up
-    create_table :jobs do |t|
+    create_table :jobber_jobs do |t|
+      t.integer  :priority, :default => 0
+      t.integer  :attempts, :default => 0
+      t.string   :type
+      t.text     :data
+
+      t.datetime :run_at
+      t.datetime :locked_at
+      t.string   :locked_by
 
       t.timestamps
     end
   end
 
   def self.down
-    drop_table :jobs
+    drop_table :jobber_jobs
   end
 end
